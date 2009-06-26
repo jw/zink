ff4cc7b432695e74d47d3b903482ea500f7b644d
jw <devnull@localhost>
2009-06-26 22:53:33 +0000

    Updated the base template. Added a message block (for issue #5).

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 
 from elevenbits.blog.models import Entry
+from elevenbits.blog.models import Tag
 from elevenbits.menu.models import Menu
 from elevenbits.page.models import Page
 
@@ -24,6 +25,7 @@ def detail(request, id):
     page = Page.objects.get(title="ElevenBits")
     menu_list = Menu.objects.all()
     entry = get_object_or_404(Entry, pk=id)
+    page.header = entry.title
     return render_to_response('detail.html', 
                               {'page': page,
                                'menu_list': menu_list, 
@@ -33,7 +35,13 @@ def detail(request, id):
 def tags(request, tag):
     page = Page.objects.get(title="ElevenBits")
     menu_list = Menu.objects.all()
-    latest_entry_list = Entry.objects.filter(tags__tag=tag).reverse()
+    latest_entry_list = Entry.objects.filter(tags__pk=tag).reverse()
+    try:
+        tag = Tag.objects.get(id=tag)
+        # TODO: refactor this!
+        page.header = str(latest_entry_list.count()) + " entries tagged with '" + tag.tag + "'"
+    except Tag.DoesNotExist:
+        page.header = "Tag with id " + tag + " not found."
     return render_to_response('index.html', 
                               {'page': page,
                                'menu_list': menu_list, 
