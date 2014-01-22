e6b447bd8d406a695bfb1bfcce1b6c6617ab4c41
Jan Willems <jan.willems@elevenbits.com>
2014-01-22 07:21:25 +0100

    The deployment module (in the elevenbits folder) is now also pep8 compliant.

--- elevenbits/deployment/models.py
+++ elevenbits/deployment/models.py
@@ -6,13 +6,13 @@ class Deployment(models.Model):
 
     tag = models.CharField('tag', max_length=255,
                            help_text='The tag of this deployment.')
-    
+
     version = models.CharField('version', max_length=255)
-    
+
     timestamp = models.DateTimeField(unique=True)
-    
+
     deployer = models.CharField(max_length=255)
-    
+
     def __unicode__(self):
         return self.version
 
--- elevenbits/deployment/tests.py
+++ elevenbits/deployment/tests.py
@@ -7,7 +7,6 @@ from elevenbits.deployment.models import Deployment
 
 
 class DeploymentTest(TestCase):
-
     def setUp(self):
         self.client = Client()
 
@@ -19,29 +18,37 @@ class DeploymentTest(TestCase):
 
 
 class RelativeTimeTest(TestCase):
-    
     def setUp(self):
         now = datetime.now()
-        self.tensecondsago = Deployment.objects.create(timestamp=(now - timedelta(seconds=10)))
-        self.fourtytwosecondsago = Deployment.objects.create(timestamp=(now - timedelta(seconds=42)))
-        self.threeminutesago = Deployment.objects.create(timestamp=(now - timedelta(minutes=3)))
-        self.threehoursago = Deployment.objects.create(timestamp=(now - timedelta(hours=3)))
-        self.fiftyhoursago = Deployment.objects.create(timestamp=(now - timedelta(hours=50)))
-        self.twoweeksago = Deployment.objects.create(timestamp=(now - timedelta(weeks=2)))
-        self.fourweeksago = Deployment.objects.create(timestamp=(now - timedelta(weeks=4)))
-        self.fourmonthsago = Deployment.objects.create(timestamp=(now - timedelta(weeks=4*4)))
+        self.tensecondsago = Deployment.objects.create(
+            timestamp=(now - timedelta(seconds=10)))
+        self.fourtytwosecondsago = Deployment.objects.create(
+            timestamp=(now - timedelta(seconds=42)))
+        self.threeminutesago = Deployment.objects.create(
+            timestamp=(now - timedelta(minutes=3)))
+        self.threehoursago = Deployment.objects.create(
+            timestamp=(now - timedelta(hours=3)))
+        self.fiftyhoursago = Deployment.objects.create(
+            timestamp=(now - timedelta(hours=50)))
+        self.twoweeksago = Deployment.objects.create(
+            timestamp=(now - timedelta(weeks=2)))
+        self.fourweeksago = Deployment.objects.create(
+            timestamp=(now - timedelta(weeks=4)))
+        self.fourmonthsago = Deployment.objects.create(
+            timestamp=(now - timedelta(weeks=4 * 4)))
         # a year is longer than 52 weeks it seems - but 55 will do
-        self.twoyearsago = Deployment.objects.create(timestamp=(now - timedelta(weeks=55*2)))
-        
+        self.twoyearsago = Deployment.objects.create(
+            timestamp=(now - timedelta(weeks=55 * 2)))
+
     def test_relative_time_tensecondsago(self):
         """
         After ten seconds, the relative_time tag should
         just show 'moments ago'
         """
-        out = Template("{% load relative_time %}"
-                       "{{deployment.timestamp|relative_time}}"
-                       ).render(Context({'deployment':
-                                         self.tensecondsago}))
+        out = Template(
+            "{% load relative_time %}"
+            "{{deployment.timestamp|relative_time}}"
+        ).render(Context({'deployment': self.tensecondsago}))
         self.assertEqual(out, "moments ago")
 
     def test_relative_time_fourtytwosecondsago(self):
@@ -49,10 +56,10 @@ class RelativeTimeTest(TestCase):
         After 42 seconds, the relative_time tag should
         just show 'about 42 seconds ago'
         """
-        out = Template("{% load relative_time %}"
-                       "{{deployment.timestamp|relative_time}}"
-                       ).render(Context({'deployment':
-                                         self.fourtytwosecondsago}))
+        out = Template(
+            "{% load relative_time %}"
+            "{{deployment.timestamp|relative_time}}"
+        ).render(Context({'deployment': self.fourtytwosecondsago}))
         self.assertEqual(out, "42 seconds ago")
 
     def test_relative_time_threeminutesago(self):
@@ -61,55 +68,73 @@ class RelativeTimeTest(TestCase):
         just show 'about 3 minutes ago'
         """
         out = Template(
-                "{% load relative_time %}"
-                "{{deployment.timestamp|relative_time}}"
-            ).render(Context({'deployment':self.threeminutesago}))
+            "{% load relative_time %}"
+            "{{deployment.timestamp|relative_time}}"
+        ).render(Context({'deployment': self.threeminutesago}))
         self.assertEqual(out, "3 minutes ago")
 
     def test_relative_time_threehoursago(self):
-        "After 3 hours, the relative_time tag should just show 'about 3 hours ago'"
+        """
+        After 3 hours, the relative_time tag should
+        just show 'about 3 hours ago'
+        """
         out = Template(
-                "{% load relative_time %}"
-                "{{deployment.timestamp|relative_time}}"
-            ).render(Context({'deployment':self.threehoursago}))
+            "{% load relative_time %}"
+            "{{deployment.timestamp|relative_time}}"
+        ).render(Context({'deployment': self.threehoursago}))
         self.assertEqual(out, "3 hours ago")
 
     def test_relative_time_fiftyhoursago(self):
-        "After 50 hours, the relative_time tag should just show 'about 2 days ago'"
+        """
+        After 50 hours, the relative_time tag should
+        just show 'about 2 days ago'
+        """
         out = Template(
-                "{% load relative_time %}"
-                "{{deployment.timestamp|relative_time}}"
-            ).render(Context({'deployment':self.fiftyhoursago}))
+            "{% load relative_time %}"
+            "{{deployment.timestamp|relative_time}}"
+        ).render(Context({'deployment': self.fiftyhoursago}))
         self.assertEqual(out, "2 days ago")
 
     def test_relative_time_twoweeksago(self):
-        "After two weeks, the relative_time tag should just show 'about 14 days ago'"
+        """
+        After two weeks, the relative_time tag should
+        just show 'about 14 days ago'"
+        """
         out = Template(
-                "{% load relative_time %}"
-                "{{deployment.timestamp|relative_time}}"
-            ).render(Context({'deployment':self.twoweeksago}))
+            "{% load relative_time %}"
+            "{{deployment.timestamp|relative_time}}"
+        ).render(Context({'deployment': self.twoweeksago}))
         self.assertEqual(out, "14 days ago")
 
     def test_relative_time_fourweeksago(self):
-        "After four weeks, the relative_time tag should just show 'about 4 weeks ago'"
+        """
+        After four weeks, the relative_time tag should
+        just show 'about 4 weeks ago'
+        """
         out = Template(
-                "{% load relative_time %}"
-                "{{deployment.timestamp|relative_time}}"
-            ).render(Context({'deployment':self.fourweeksago}))
+            "{% load relative_time %}"
+            "{{deployment.timestamp|relative_time}}"
+        ).render(Context({'deployment': self.fourweeksago}))
         self.assertEqual(out, "4 weeks ago")
 
     def test_relative_time_fourmonthsago(self):
-        "After four months, the relative_time tag should just show 'about 4 months ago'"
+        """
+        After four months, the relative_time tag should
+        just show 'about 4 months ago'
+        """
         out = Template(
-                "{% load relative_time %}"
-                "{{deployment.timestamp|relative_time}}"
-            ).render(Context({'deployment':self.fourmonthsago}))
+            "{% load relative_time %}"
+            "{{deployment.timestamp|relative_time}}"
+        ).render(Context({'deployment': self.fourmonthsago}))
         self.assertEqual(out, "4 months ago")
 
     def test_relative_time_twoyearsago(self):
-        "After two years, the relative_time tag should just show 'about 2 years ago'"
+        """
+        After two years, the relative_time tag should
+        just show 'about 2 years ago'
+        """
         out = Template(
-                "{% load relative_time %}"
-                "{{deployment.timestamp|relative_time}}"
-            ).render(Context({'deployment':self.twoyearsago}))
+            "{% load relative_time %}"
+            "{{deployment.timestamp|relative_time}}"
+        ).render(Context({'deployment': self.twoyearsago}))
         self.assertEqual(out, "2 years ago")
