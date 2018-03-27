2f147fe7c02c0a0b759592a51b5deb4e991c8133
Jan Willems <jw@elevenbits.com>
2018-03-27 20:34:16 +0200

    Updated the Django version to 2.0.3 and upgrade the Reading app model.

--- reading/models.py
+++ reading/models.py
@@ -23,35 +23,54 @@ class Text(models.Model):
     created = models.DateTimeField('created',
                                    help_text='Date and time when '
                                              'this text came into my hands.',
-                                   default=now)
+                                   default=now, null=True, blank=True)
 
-    read = models.DateTimeField('created',
-                                help_text='Date and time when '
-                                          'this text was read.',
-                                default=now)
+    started_reading = models.DateTimeField('started_reading',
+                                           help_text='Date and time when '
+                                                     'I started to read '
+                                                     'this text.',
+                                           default=now)
+
+    finished_reading = models.DateTimeField('finished_reading',
+                                            help_text='Date and time when '
+                                                      'I finished reading '
+                                                      'this text.',
+                                            default=now, null=True, blank=True)
 
     reading = models.BooleanField(default=False,
                                   help_text='Reading this next now.')
+
+    completed = models.BooleanField(default=False,
+                                    help_text='Read the text completely.')
 
     title = models.TextField(help_text="Title of the text.")
 
     author = models.TextField(help_text="Authors of the text.")
 
-    review = models.TextField(help_text="What you thought of it.")
-
-    type = models.ManyToManyField(Type, blank=True)
+    type = models.ManyToManyField(Type, blank=True,
+                                  help_text="The type of reading material.")
 
     pages = models.IntegerField(help_text="The number of pages.")
 
     current_page = models.IntegerField(help_text="The current page.")
 
-    topics = models.ManyToManyField(Topic, blank=True)
+    topics = models.ManyToManyField(Topic, blank=True,
+                                    help_text="The topic this is about.")
+
+    review = models.TextField(help_text="What you thought of it.")
 
     ranking = models.IntegerField(validators=[
         MaxValueValidator(10),
         MinValueValidator(-1)
-    ])
+    ], help_text="Its rank - how good was it?")
 
     def __str__(self):
+        if self.reading:
+            return f'{self.title} by {self.author} ' \
+                   f'({self.current_page}/{self.pages}) ; reading it now.'
+        if self.completed:
+            return f'{self.title} by {self.author} ' \
+                   f'({self.current_page}/{self.pages}).' \
+                   f'; read it at {self.completed}.'
         return f'{self.title} by {self.author} ' \
-               f'({self.current_page}/{self.pages})'
+               f'({self.current_page}/{self.pages}).'
