921bbdac2bad056fa7e010ecc25447f8933bbb6d
Jan Willems <jw@elevenbts.com>
2020-04-06 22:14:42 +0200

    Added less based colouring, added fonts, cleaned up index title and scrollup botton.

---
+++ index.js
@@ -0,0 +1,8 @@
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+// components can be called from the imported UIkit reference
+UIkit.notification('Hello world.');
