a8c20f9bc28d2fc7bec7ae9a30356c80fa88d9fb
Jan Willems <jw@elevenbts.com>
2020-06-06 07:57:52 +0200

    Debug info in deployment tasks.

--- deployment-tasks.sh
+++ deployment-tasks.sh
@@ -6,7 +6,13 @@ env
 poetry -V
 poetry config --local --list
 poetry config --list
+
 poetry env info
+
+python -V
+which python
+
+ls -al `poetry config virtualenvs.path`
 
 poetry run python manage.py collectstatic --no-input --clear
 poetry run python manage.py compress -v 2
