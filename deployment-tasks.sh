66bc9f1a72c6ac7f074815a13b2ad508f2521ff8
Jan Willems <jw@elevenbts.com>
2020-06-06 07:35:53 +0200

    Debug info in deployment tasks.

--- deployment-tasks.sh
+++ deployment-tasks.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
+echo $PYTHONPATH
+echo $PATH
+env
+
+poetry -V
+poetry --local --list
+poetry --list
+poetry env info
+
 poetry run python manage.py collectstatic --no-input --clear
 poetry run python manage.py compress -v 2
