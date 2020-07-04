233f6288c30ec1d76415b9af0c726cb907b57e6e
Jan Willems <jw@elevenbts.com>
2020-07-04 17:12:38 +0200

    Emptied project.

--- index.js
+++
@@ -1,8 +0,0 @@
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
-
-// loads the Icon plugin
-UIkit.use(Icons);
-
-// components can be called from the imported UIkit reference
-UIkit.notification('Hello world.');
--- zink/admin.py
+++
@@ -1,4 +0,0 @@
-from zink.models import Deployment
-from django.contrib import admin
-
-admin.site.register(Deployment)
