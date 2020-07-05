1f5131fdb4e25b117b3b411ac1e2a9e182b0f07d
Jan Willems <jw@elevenbts.com>
2020-07-05 10:17:16 +0200

    Sheing debuging mode in stilus.

--- zink/views.py
+++ zink/views.py
@@ -2,11 +2,14 @@ import logging
 
 from django.shortcuts import render
 
+from zink import settings
+
 log = logging.getLogger(__name__)
 
 
 def stilus(request):
 
-    attributes = {'stilus': "Stilus!",}
+    attributes = {'stilus': "Stilus!",
+                  'debug': settings.DEBUG}
 
     return render(request, 'stilus.html', attributes)
