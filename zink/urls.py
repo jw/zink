ce506c5471d578187cadebdc76a4fe2c740d9126
Jan Willems <jw@elevenbts.com>
2020-07-07 21:27:40 +0200

    Added home page.

--- zink/urls.py
+++ zink/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from zink.views import stilus
+from zink.views import home, stilus
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('stilus/', stilus, name="stilus"),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('admin/', admin.site.urls),
+
 ]
--- zink/views.py
+++ zink/views.py
@@ -4,9 +4,42 @@ from pathlib import Path
 
 from django.shortcuts import render
 
+from blog.models import Menu, Entry
+from blog.views import get_assets, create_menus
 from zink import settings
 
 logger = logging.getLogger(__name__)
+
+
+def home(request):
+    """Show the home page."""
+
+    assets = get_assets(prefix="index")
+
+    menus = create_menus(Menu.roots[0])
+    logger.info(f"Retrieved {len(menus)} menu items.")
+
+    entry_list = Entry.objects.filter(page=Entry.BLOG, active=True).reverse()
+    logger.info(f"Retrieved {len(entry_list)} blog entries.")
+
+    # books = list(Text.objects.filter(reading=True))
+    # if books:
+    #     logger.info(f"Reading one (or more) books: {books}.")
+    # else:
+    #     logger.info("Not reading any book! So sad.")
+
+    try:
+        entry = entry_list.first()
+    except IndexError:
+        entry = None
+
+    attributes = {'entry': entry,
+                  'entries': entry_list[1:],
+                  # 'books': books,
+                  'menus': menus,
+                  'assets': assets}
+
+    return render(request, 'index.html', attributes)
 
 
 def stilus(request):
