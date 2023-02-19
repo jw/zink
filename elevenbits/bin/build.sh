2cd29f07d1e3fa545db475b19dc45d642130e284
Jan Willems <jan.willems@elevenbits.com>
2023-02-19 10:32:36 +0100

    fix: Moved the build/tailwindcss scripts to the build folder.

--- elevenbits/bin/build.sh
+++ elevenbits/bin/build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 poetry install
 
-./bin/tailwindcss -i input.css -o output.css --minify
+bin/tailwindcss -i static/tailwind/input.css -o static/tailwind/output.css --minify
+
 python manage.py collectstatic --no-input
 python manage.py migrate
