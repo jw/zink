352c9e78d4af32f917f3e8f6047cbb08bf49e87a
Jan Willems <jan.willems@elevenbits.com>
2023-02-26 18:42:48 +0100

    feature: Forced DEBUG to be False in production.

--- elevenbits/settings.py
+++ elevenbits/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("RENDER", default=os.environ.get("DEBUG", default=False))
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = []
 
