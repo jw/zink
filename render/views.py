04c814aa66cc87fc90cbdc544a4bc5e5560cce50
Jan Willems <jan.willems@elevenbits.com>
2023-03-19 09:32:15 +0100

    feat: Added hero and footer to the index page.  Also created a very basic about and cookies pages.

--- render/views.py
+++ render/views.py
@@ -10,4 +10,19 @@ logger = logging.getLogger("zink")
 def index(request):  # noqa: ANN001
     entry_list = Entry.objects.filter(page=Entry.BLOG, active=True).reverse()
     logger.warning(f"Retrieved {len(entry_list)} blog entries.")
-    return render(request, "index.html", {})
+
+    attributes = {
+        "entry": entry_list[0],
+    }
+
+    return render(request, "index.html", attributes)
+
+
+def about(request):
+    logger.warning("About!")
+    return render(request, "about.html", {})
+
+
+def cookies(request):
+    logger.warning("Cookies!")
+    return render(request, "cookies.html", {})
