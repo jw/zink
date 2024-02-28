b1bcce059b85d28547e6b1b52a92c752fdf95211
Jan Willems <jw@elevenbits.com>
2024-02-28 19:22:30 +0100

    feat: Added tables to the markdown.

--- core/templatetags/markdownify.py
+++ core/templatetags/markdownify.py
@@ -9,5 +9,5 @@ register = template.Library()
 @register.filter
 @stringfilter
 def markdown(value: str) -> str:
-    md = markdownify.Markdown(extensions=["fenced_code", "codehilite"])
+    md = markdownify.Markdown(extensions=["fenced_code", "tables", "codehilite"])
     return mark_safe(md.convert(value))
